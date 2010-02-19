3058314a840b5fa9c7616f30ca3430f1403d26fc
Tom Crinson <the.jaba@gmail.com>
2010-02-19 10:23:17 +0000

    [SCR-614] + others. Our lightboxes do not gain focus when opening. Using these exensions and adding the appropriate class now allows this

---
+++ lightbox_extensions.js
@@ -0,0 +1,31 @@
+AegisLabs = window.AegisLabs || {}
+AegisLabs.LightBoxExtensions = {
+	
+	widestChild : function (element, widths){
+		if( $(element).childElements().size() > 0 ){ 
+			$(element).childElements().each( function(element){  widths.push( widestChild( element, widths ) ) } );
+		}
+		else{
+			return $(element).getDimensions().width
+		}
+	},
+	
+	//Add class 'lbResizable' to the lightbox link to work
+	resizeLightBox : function(){
+		var widths = new Array();
+		AegisLabs.LightBoxExtensions.widestChild( $('lightbox'), widths );
+		var widest = widths.sortBy( function(width){ 
+			return width == null ? 0 : parseInt(width);
+			} ).last();
+			widest += 50;
+			$('lightbox').morph('width:'+widest+'px;'); 
+			$('lightbox').morph('height:550px;'); 
+		},
+		
+	//Add class 'lbStealFocus' to the lightbox link to work
+	setFocusToForm : function(){
+		$$('#lightbox form').first().focusFirstElement();
+	}
+	
+}
+
